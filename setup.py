0a6f35405f51ad9e50448f4a26449ab010bf8941
k-dominik <k-dominik@users.noreply.github.com>
2022-02-22 11:35:03 +0100

    add parametrization

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
+import os
 from distutils.core import setup
+
+scritps = ["hello_you"] if not os.environ.get("SCRIPT_WITH_PY") else ["hello_you.py"]
+
 
 setup(name='fortytwo',
       version='1.0',
+      packages=["fortytwo"],
+      scripts=scripts
 )
